ec1cf24bf63945117e4651f52425dca39133d814
Igor Deminski <kartoschka17gmail.com>
2022-05-06 09:51:36 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Sound Cloud
+The Sound Cloud project is an audio platform that allows people to find, listen to and download music. Musicians can download music for free use.
+
+Functional
+Authorization via Google and Spotify
+Editing a user profile
+Create, edit and delete
+    Albums
+    Playlists
+    Tracks
+    Licenses
+Download, play and download music
+Adding an artist to favorites
+Track comments
---
+++ src/oauth/endpoint/views.py
@@ -0,0 +1,35 @@
+from rest_framework import viewsets, parsers, permissions
+
+from .. import serializer, models
+from ...base.permissions import IsAuthor
+
+
+class UserView(viewsets.ModelViewSet):
+    """ Просмотр и редактирование данных пользователя
+    """
+    parser_classes = (parsers.MultiPartParser,)
+    serializer_class = serializer.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user
+
+    def get_object(self):
+        return self.get_queryset()
+
+
+class AuthorView(viewsets.ModelViewSet):
+    queryset = models.AuthUser.objects.all().prefetch_related('social_links')
+    serializer_class = serializer.AuthorSerializer
+
+
+class SocialLinkView(viewsets.ModelViewSet):
+    serializer_class = serializer.SocialLinkSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return self.request.user.social_links.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
